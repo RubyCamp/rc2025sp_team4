ddd83a00e3b79d0d2317d8f9636cecd283cf1184
Fukuta Habashima <kame.fukuta@gmail.com>
2025-03-05 15:21:28 +0900

    とりあえず完成

--- route.rb
+++ route.rb
@@ -9,12 +9,19 @@ lm_pin2 = PWM.new(33) # 左モーターPIN2
 lux_right = ADC.new(35) # 右ライトセンサー初期化（GPIO番号: 35）
 lux_left  = ADC.new(2)  # 左ライトセンサー初期化（GPIO番号: 2）
 
+lm_pin1.duty(100)
+lm_pin2.duty(65)
+rm_pin1.duty(100)
+rm_pin2.duty(65)
+
+sleep 1.5
+
 sleeptime=0
-while sleeptime<1.5
-  lm_pin1.duty(30)
-  lm_pin2.duty(00)
-  rm_pin1.duty(33)
-  rm_pin2.duty(00)
+while sleeptime < 1.5
+  lm_pin1.duty(100)
+  lm_pin2.duty(70)
+  rm_pin1.duty(100)
+  rm_pin2.duty(70)
   puts "light_left: #{lux_left.read_raw}"
 
   sleep 1
@@ -25,8 +32,8 @@ while sleeptime<1.5
 
   sleep 1
   if (lux_left.read_raw >= 0 && lux_left.read_raw < 300)
-    lm_pin1.duty(30)
-    lm_pin2.duty(00)
+    lm_pin1.duty(100)
+    lm_pin2.duty(70)
     rm_pin1.duty(100)
     rm_pin2.duty(100)
     sleep 0.5
@@ -36,20 +43,28 @@ while sleeptime<1.5
 end
 
 # 右に45度回転
-lm_pin1.duty(30)
-lm_pin2.duty(00)
-rm_pin1.duty(00)
-rm_pin2.duty(28)
+lm_pin1.duty(100)
+lm_pin2.duty(70)
+rm_pin1.duty(70)
+rm_pin2.duty(100)
 
-sleep 0.8
+sleep 0.7
 
 # 直進
-lm_pin1.duty(30)
-lm_pin2.duty(00)
-rm_pin1.duty(30)
-rm_pin2.duty(00)
+lm_pin1.duty(100)
+lm_pin2.duty(70)
+rm_pin1.duty(100)
+rm_pin2.duty(70)
 
-sleep 2
+sleep 2.7
+
+# Aの方角を向く
+lm_pin1.duty(100)
+lm_pin2.duty(70)
+rm_pin1.duty(70)
+rm_pin2.duty(100)
+
+sleep 0.9
 
 # 左右モーター停止
 lm_pin1.duty(100)
